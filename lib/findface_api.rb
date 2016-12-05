cb22477ab8108a41f1eaedb42c41232d8cd8bd51
Alexander Kovalenko <alexanderk23@gmail.com>
2016-12-05 12:01:24 +0300

    Add custom exception

--- lib/findface_api.rb
+++ lib/findface_api.rb
@@ -7,6 +7,11 @@ require 'findface_api/version'
 module FindfaceApi
   ENDPOINT_URI = "https://api.findface.pro/v#{API_VERSION}/".freeze
 
+  # Exceptions
+  module Error
+    class ClientError < RuntimeError; end
+  end
+
   # Configuration
   module Configuration
     attr_accessor :access_token, :proxy, :logger, :adapter
@@ -25,7 +30,6 @@ module FindfaceApi
           c.authorization :Token, access_token
           c.request :multipart
           c.request :json # either :json or :url_encoded
-          c.response :raise_error
           c.response :logger, logger, headers: false, bodies: true unless logger.nil?
           c.response :json, content_type: /\bjson$/
           c.proxy proxy unless proxy.nil?
@@ -42,11 +46,21 @@ module FindfaceApi
     # Represents a rectangle on a photo. Usually used as a face's bounding box.
     class BBox
       attr_accessor :x1, :x2, :y1, :y2
+
       def initialize(x1:, x2:, y1:, y2:)
         @x1, @x2, @y1, @y2 = x1, x2, y1, y2
       end
+
+      def width
+        x2 - x1
+      end
+
+      def height
+        y2 - y1
+      end
+
       def to_h
-        { x1: x1, x2: x2, y1: y1, y2: y2}
+        { x1: x1, x2: x2, y1: y1, y2: y2 }
       end
     end
 
@@ -58,6 +72,7 @@ module FindfaceApi
     end
   end
 
+  # Helpers
   module Helpers
     def symbolize(myhash)
       myhash.keys.each do |key|
@@ -76,13 +91,21 @@ module FindfaceApi
         .gsub(':gallery', options.fetch(:gallery, :default).to_s)
         .gsub(':meta', options.fetch(:meta, '').to_s)
     end
+
+    def post(uri, data)
+      response = connection.post uri, data
+      if !response.success? || response.body.include?('code')
+        raise FindfaceApi::Error::ClientError, response.body
+      end
+      response.body
+    end
   end
 
   # API Methods
   module APIMethods
     def detect(photo)
-      response = connection.post('detect/', photo: photo)
-      response.body['faces'].map do |box|
+      response = post('detect/', photo: photo)
+      response['faces'].map do |box|
         ::FindfaceApi::Entities::BBox.new(**symbolize(box))
       end
     end
@@ -90,15 +113,14 @@ module FindfaceApi
     def verify(photo1, photo2, **options)
       keys = %i(bbox1 bbox2 threshold mf_selector)
       payload = request_body(keys, options, photo1: photo1, photo2: photo2)
-      response = connection.post('verify/', payload)
-      response.body
+      post('verify/', payload)
     end
 
     def identify(photo, **options)
       keys = %i(bbox threshold n mf_selector)
       payload = request_body(keys, options, photo: photo)
       path = request_path('faces/gallery/:gallery/identify/', options)
-      response = connection.post(path, payload)
+      response = post(path, payload)
       response.body['results']
     end
   end
